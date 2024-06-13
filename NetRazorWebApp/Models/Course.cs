f1e24d762a8e1f02d71113d362df6f758a4e3a8c
EngelReyes23 <EngelReyes23@Gmail.com>
2024-06-12 21:05:24 -0600

    Add Course model

---
+++ NetRazorWebApp/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetRazorWebApp.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Course Name")]
+        public required string CourseName { get; set; }
+
+        [Display(Name = "Quantity of Classes")]
+        public int QuantityClasses { get; set; }
+
+        [Display(Name = "Price of the course")]
+        public double Price { get; set; }
+
+        [Display(Name = "Date of Creation")]
+        public required DateTime DateCreated { get; set; }
+    }
+}
