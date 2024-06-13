d761f0c3432e7319417e50a6d489c66699a5a48e
EngelReyes23 <EngelReyes23@Gmail.com>
2024-06-12 20:47:05 -0600

    AplicationDBContext inherits DbContext for database operations setup.

---
+++ NetRazorWebApp/Data/AplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetRazorWebApp.Data
+{
+    public class AplicationDBContext: DbContext
+    {
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options): base(options)
+        {
+            
+        }
+    }
+}
